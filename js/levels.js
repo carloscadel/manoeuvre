12311084c487d1446371511f7f041020e56bcc50
carloscadel <c.cazallas.delfa@gmail.com>
2018-10-24 19:20:19 +0200

    Design Level 2

--- js/levels.js
+++ js/levels.js
@@ -1,8 +1,9 @@
 // widthxheight = 1400x700
 
-var levels = [
+var levels = 
+[
     {
-        name: "Level 0",
+        name: "Intro",
         
         spaceship: {x: 1/9, 
                     y: 1/9,
@@ -15,7 +16,7 @@ var levels = [
 
         planets: [{ x: 1/2, 
                     y: 1/2, 
-                    radius: 75,
+                    radius: 20,
                     vx: 0,
                     vy: 0, 
                     color: 'rgb(114, 158, 161)',
@@ -23,53 +24,31 @@ var levels = [
                     isTarget: false,
                     hasGravity: true}],
 
-        sats:  [{   name: "sat1",
-                    x: 1/4, 
-                    y: 1/2, 
-                    radius: 10,
-                    vx: 0,
-                    vy: -5,
-                    color: 'rgb(255, 107, 53)',
-                    fixedPos: false,
-                    isTarget: false,
-                    hasGravity: false,
-                    assignedGoal: undefined
-                },
-                {   name: "sat2",
-                    x: 1/2, 
-                    y: 1/4, 
-                    radius: 10,
-                    vx: 7,
-                    vy: 0,
-                    color: 'rgb(255, 107, 53)',
-                    fixedPos: false,
-                    isTarget: false,
-                    hasGravity: false,
-                    assignedGoal: undefined
-                },
-                {   name: "sat3",
-                    x: 3/4,
-                    y: 1/2,
-                    radius: 10,
-                    vx: 0,
-                    vy: 5,
-                    color: 'rgb(255, 107, 53)',
-                    fixedPos: false,
-                    isTarget: false,
-                    hasGravity: false,
-                    assignedGoal: undefined
-                },
-                {   name: "sat4",
+        sats:  [
+                {   name: "sat1",
                     x: 1/2,    
-                    y: 3/4,
+                    y: 1/4,
                     radius: 10,
-                    vx: -7,
+                    vx: 3,
                     vy: 0,
                     color: 'rgb(152, 206, 0)',
                     fixedPos: false,
                     isTarget: true,
                     hasGravity: false,
+                    assignedGoal: 0},
+
+                {   name: "sat2",
+                    x: 1/2,    
+                    y: 3/4,
+                    radius: 10,
+                    vx: -3,
+                    vy: 0,
+                    color: 'rgb(255, 107, 53)',
+                    fixedPos: false,
+                    isTarget: false,
+                    hasGravity: false,
                     assignedGoal: 0}],
+
         goals: [{   name: 'goal0',
                     x: 7/8, 
                     y: 1/8, 
@@ -77,7 +56,8 @@ var levels = [
                     color: 'rgba(0, 255, 0, 0.3)'
                 }]
                     
-    }, {
+    }, 
+    {
         name: "Level 1",
         
         spaceship: {x: 1/9, 
@@ -155,4 +135,85 @@ var levels = [
                     color: 'rgba(0, 255, 0, 0.3)'
                 }]
                     
-    }]
+    },
+    {
+        name: "Level 2",
+        
+        spaceship: {x: 1/9, 
+                    y: 1/9,
+                    size: 10,
+                    vx: 0,
+                    vy: 0,
+                    color: 'grey',
+                    fixedPos: false
+                },
+
+        planets: [{ x: 0.7, 
+                    y: 0.7, 
+                    radius: 75,
+                    vx: 0,
+                    vy: 0, 
+                    color: 'rgb(114, 158, 161)',
+                    fixedPos: true,
+                    isTarget: false,
+                    hasGravity: true}
+
+                ],
+
+        sats:  [{   name: "sat1",
+                    x: 0.7, 
+                    y: 1.0, 
+                    radius: 10,
+                    vx: -5,
+                    vy: 0,
+                    color: 'rgb(255, 107, 53)',
+                    fixedPos: false,
+                    isTarget: false,
+                    hasGravity: false,
+                    assignedGoal: undefined
+                },
+                {   name: "sat2",
+                    x: 1.0, 
+                    y: 0.7, 
+                    radius: 10,
+                    vx: 0,
+                    vy: 5,
+                    color: 'rgb(255, 107, 53)',
+                    fixedPos: false,
+                    isTarget: false,
+                    hasGravity: false,
+                    assignedGoal: undefined
+                },
+                {   name: "sat3",
+                    x: 0.7,
+                    y: 0.3,
+                    radius: 10,
+                    vx: -5,
+                    vy: 0,
+                    color: 'rgb(152, 206, 0)',
+                    fixedPos: false,
+                    isTarget: true,
+                    hasGravity: false,
+                    assignedGoal: 0
+                },
+                {   name: "sat4",
+                    x: 0.7,    
+                    y: 0.5,
+                    radius: 10,
+                    vx: -5,
+                    vy: 0,
+                    color: 'rgb(255, 107, 53)',
+                    fixedPos: false,
+                    isTarget: false,
+                    hasGravity: false,
+                    assignedGoal: undefined}],
+
+        goals: [{   name: 'goal0',
+                    x: 0.2, 
+                    y: 0.2, 
+                    radius: 30,
+                    color: 'rgba(0, 255, 0, 0.3)'
+                }]
+                    
+    }
+]
--- js/main.js
+++ js/main.js
@@ -6,15 +6,15 @@ ctx.canvas.width = window.innerWidth
 ctx.canvas.height = window.innerHeight
 var width = canvas.width
 var height = canvas.height
-var gravConst = 5
+var gravConst = 3
 // var gravConst = 6.67e10-11
 const pi = Math.PI
 
 
 // var game = new Game(ctx, levels[0])
 
-var game = new Game(ctx, levels[1])
-// var game = new Game(ctx, levels[2])
+// var game = new Game(ctx, levels[1])
+var game = new Game(ctx, levels[2])
 // var game = new Game(ctx, levels[3])
 
 
